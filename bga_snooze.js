d1e66734e31dd14288f939a01ddbcbbe7880880f
jonpbak <jonpbak@vt.edu>
2023-07-18 23:23:03 -0600

    Snooze all games of a type at once

--- bga_snooze.js
+++ bga_snooze.js
@@ -5,7 +5,7 @@
 // @description  Add snooze button to BGA tables
 // @author       Babamots
 // @match        *://boardgamearena.com/gameinprogress*
-// @match        *://boardgamearena.com/1/*?table=*
+// @match        *://boardgamearena.com/1*/*?table=*
 // @match        *://boardgamearena.com/2/*?table=*
 // @match        *://boardgamearena.com/3/*?table=*
 // @match        *://boardgamearena.com/4/*?table=*
@@ -25,7 +25,6 @@ var debugging = 0;
 // https://github.com/Tampermonkey/tampermonkey/issues/1279
 // It calls callback() function when the page is fully loaded as indicated by the query readySelector finding something
 // This is needed on the list page because the table info is loaded by JS after the signal Tampermonkey uses to start running
-// This worked for a while, but now it doesn't seem to
 function run_when_ready(readySelector, callback) {
     var numAttempts = 0;
     var tryNow = function() {
@@ -64,6 +63,10 @@ function snooze_cookie_name(table){
     // Name of the cookie indicating that this table is snoozed
     return 'snooze_table_' + table;
 }
+function snooze_game_cookie_name(){
+    // Name of the cookie listing names of snoozed games
+    return 'snooze_games';
+}
 function queue_cookie_name(){
     // Name of the cookie indicating that this table is snoozed
     return 'table_queue';
@@ -75,10 +78,14 @@ function queue_cookie_name(){
 function add_table_buttons(){
     // Add buttons to display on table pages
 
-    let bar = document.getElementById('maintitlebar_content');
+    //let bga_container_id = 'maintitlebar_content';
+    //let bga_container_id = 'current_header_infos_wrap';
+    let bga_container_id = 'right-side';
+    let bar = document.getElementById(bga_container_id);
 
     let container = document.createElement('div');
-    container.setAttribute('style','right:0px;float:right');
+    //container.setAttribute('style','right:0px;float:right');
+    //container.setAttribute('style','margin-left:180px');
     container.id = 'snooze_container';
     bar.insertBefore(container,bar.firstChild);
 
@@ -98,6 +105,15 @@ function add_table_buttons(){
     button.setAttribute('style',"background-color:black;margin:5px");
     button.setAttribute('onclick','window.snooze_functions.snooze_clicked()');
     button.innerText = 'Snooze 1 hr';
+    container.appendChild(button);
+
+    // Snooze all button
+    button = document.createElement('a');
+    button.id = 'snooze_all_button';
+    button.setAttribute('class','bgabutton');
+    button.setAttribute('style',"background-color:black;margin:5px");
+    button.setAttribute('onclick','window.snooze_functions.snooze_all_clicked()');
+    button.innerText = 'Snooze all matching games 1 hr';
     container.appendChild(button);
 
     // Skip button
@@ -143,18 +159,18 @@ function add_list_buttons(){
 ///////////////////////
 function set_cookie(value, minutes=null) {
 	// Set a cookie with the given value (or name-value string), expiration time (in minutes), and path=/
-    let toset = value;
+    let toset = value + ';path=/';
     if(minutes != null){
         const d = new Date();
         d.setTime(d.getTime() + (minutes*60*1000));
         toset += ';expires=' + d.toUTCString();
     }
-    toset += ';path=/';
     document.cookie = toset;
 }
 function clear_cookie(name){
     // The path of this cookie must be /
-    document.cookie = name + ';path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT';
+    let fullname = name+'='+get_cookie_value(name);
+    document.cookie = fullname + ';path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT';
 }
 function get_cookie_value(name) {
     // Get the value of the cookie with this name (or null)
@@ -172,6 +188,9 @@ function get_cookie_value(name) {
     }
     return null;
 }
+function snooze_table(table,minutes=60){
+    set_cookie(snooze_cookie_name(table)+'=y',minutes);
+}
 
 ////////////////////
 // LOCATION CHECK //
@@ -188,7 +207,7 @@ function at_list(){
     let url = window.location.href;
     return url.indexOf('/gameinprogress') > 0;
 }
-function is_snoozed(table){
+function is_snoozed_table(table){
     // Check if this table is already snoozed
     return get_cookie_value(snooze_cookie_name(table)) == 'y';
 }
@@ -201,6 +220,15 @@ function get_url_param(name, url = window.location.href) {
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+function get_game_name(url = window.location.href){
+    // Get the name of the game as it appears in the table url e.g. "homeworlds" or "thecrewdeepsea"
+    var reg = /boardgamearena.com\/\d+\/(\w+)\?table=/;
+    var m = url.match(reg);
+    if(m==null){
+        return null;
+    }
+    return m[1];
 }
 function low_time_remaining(minutes=60){
     // Is the player's time remaining less than the input number of minutes?
@@ -229,18 +257,43 @@ function low_time_remaining(minutes=60){
         return true;
     }
     return false;
-
 }
 //////////////////////
 // QUEUE MANAGEMENT //
 //////////////////////
 function build_queue(){
-    // Get the active games from the list page and store them as a list in  a session cookie
-    let nodes = Array.from(document.querySelectorAll('.tableplace_activeplayer_current'));
+    // Get the active games from the list page and store them as a list in a session cookie
+    let actives = Array.from(document.querySelectorAll('.tableplace_activeplayer_current'));
     // Example id of one of these nodes: tableplace_340479481_84983491
     // First number is the table number, second is the active player's number (the same for all these nodes)
-    let tables = nodes.map(node => node.id.split('_')[1]);
+    let tables = actives.map(node => node.id.split('_')[1]);
     set_cookie(queue_cookie_name() + '=' + tables.join(','));
+}
+function snooze_tables_of_snoozed_games(minutes){
+    // Snooze every table whose game is in the list of snoozed games
+    let c = get_cookie_value(snooze_game_cookie_name());
+    if(c == null){
+        return;
+    }
+    let names = c.split(',');
+    dprint('snoozing tables for games '+names);
+    let anchors = Array.from(document.querySelectorAll('.gametablelink'));
+    dprint('got anchors: '+anchors);
+    let urls = anchors.map(a => a.href);
+    let table,name;
+    for(let i=0; i<urls.length; i++){
+        dprint('looking at url '+urls[i]);
+        name = get_game_name(urls[i]);
+        if(!names.includes(name)){
+            dprint(name+' not in the snooze game list');
+            continue;
+        }
+        // All games with this name are supposed to be snoozed
+        table = get_url_param('table',urls[i]);
+        dprint('snoozing table '+table);
+        snooze_table(table,minutes);
+    }
+    dalert('finished snoozing tables of snoozed games');
 }
 function peek_queue(pop=false){
     // Return first table in queue
@@ -307,20 +360,20 @@ function go_next(current_table){
     }
     top = peek_queue();
     // Find the next unsnoozed table in queue
-    while(top != null && is_snoozed(top)){
+    while(top != null && is_snoozed_table(top)){
         peek_queue(true);
         top = peek_queue();
     }
     if(top==null){
         // We just emptied the queue
-        // All games are inactive or snoozed
+        // All games were inactive or snoozed when they reached the front of the queue
         if(current_table == null){
             // We are at the list, which is where BGA sends users when there are no active games
             // Just stay here
             dprint('empty queue on list');
             return;
         }
-        // We're at a table now, but we need to go to the list and rebuild the queue
+        // We're at a table now, but we need to go to the list and rebuild the queue in case we became the active player in any games
         dprint('refilling queue');
         go_list(true);
         return;
@@ -342,8 +395,34 @@ function snooze_clicked(minutes=60){
         }
     }
     let table = get_url_param('table');
-    set_cookie(snooze_cookie_name(table)+'=y',minutes);
+    snooze_table(table,minutes);
     go_next(table);
+}
+function snooze_all_clicked(minutes=60){
+    // Add a snooze cookie for this game name and go to table list so that queue can be rebuilt with all tables of this type snoozed
+    // subject to confirmation if time remaining is low
+    if(low_time_remaining(minutes)){
+        if(!confirm('It appears that you will have little or no time on your clock when the snooze expires. Are you sure this is what you want?')){
+            return;
+        }
+    }
+    let name = get_game_name();
+    if(name==null){
+        alert('Error: could not determine game name');
+    }
+    // Append game name to list of snoozed games
+    let curlist = get_cookie_value(snooze_game_cookie_name());
+    if(curlist != null && curlist.spit(',').includes(name)){
+        dalert(name+' is already on list of snoozed games');
+        return;
+    }
+    if(curlist == null){
+        set_cookie(snooze_game_cookie_name()+'='+name,minutes);
+    }
+    else{
+        set_cookie(snooze_game_cookie_name()+'='+curlist+','+name,minutes);
+    }
+    go_list(true);
 }
 function clear_all_clicked(){
     // Delete all snoozing cookies
@@ -355,6 +434,7 @@ function clear_all_clicked(){
             clear_cookie(c);
         }
     }
+    clear_cookie(snooze_game_cookie_name());
 }
 function skip_clicked(){
     if(at_list()){
@@ -369,6 +449,8 @@ function skip_clicked(){
 /////////////////
 function go_list(seeking=false,exclude_history=false){
     // Go back to the game list
+    // If seeking, then redirect to the first active non-snoozed game
+    // If exclude_history, the current page will not appear in history or be available with back button
     let dest = 'https://boardgamearena.com/gameinprogress';
     if(seeking){
         dalert('going to list, seeking');
@@ -405,8 +487,11 @@ function go_table(table,exclude_history=false){
 }
 function build_queue_and_go(exclude_history=false){
     // Run this while on list page to build the queue and redirect to table
+    dprint('building queue')
     function subfun(){
         build_queue();
+        dprint('queue built, checking snoozed games')
+        snooze_tables_of_snoozed_games(60);
         let top = peek_queue();
         go_next(null,exclude_history);
     }
@@ -414,7 +499,7 @@ function build_queue_and_go(exclude_history=false){
 }
 
 // Save the button-connected functions somewhere that remains accessible after this script runs
-window.snooze_functions = {snooze_clicked,clear_all_clicked,skip_clicked};
+window.snooze_functions = {snooze_clicked,clear_all_clicked,skip_clicked,snooze_all_clicked};
 
 (function() {
     'use strict';
@@ -443,7 +528,7 @@ window.snooze_functions = {snooze_clicked,clear_all_clicked,skip_clicked};
     // This is a table page
     var table = get_url_param('table');
 
-    if(is_snoozed(table)){
+    if(is_snoozed_table(table)){
         // Check if user really wants a snoozed table
         if(!confirm('This table is snoozed. Would you like to visit it anyway?')){
             // User declined to visit this table
